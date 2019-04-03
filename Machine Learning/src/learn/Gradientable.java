14f223aa685a4e9b46dc5fe739cf0840da939513
connor <connor@connor-briggs-xubuntu-17.10>
2019-04-02 20:24:20 -0400

    Added comments.

--- Machine Learning/src/learn/Gradientable.java
+++ Machine Learning/src/learn/Gradientable.java
@@ -6,5 +6,15 @@ package learn;
  *
  */
 public interface Gradientable extends LossFunction {
+	//TODO Make sure this works in context. 
+	/**
+	 * Returns the gradient of the loss function.
+	 * @param r
+	 * @param weight
+	 * @param bias
+	 * @param inputs
+	 * @param outputs
+	 * @return
+	 */
 	double[] loss_gradient(Regression r, double[] weight, double bias, double[][] inputs, double[] outputs);
 }
