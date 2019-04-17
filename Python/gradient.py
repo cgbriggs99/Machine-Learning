26622407e71bd5bf6b208492773c5b47d9929ec9
Connor Briggs <connor@connor-briggs-xubuntu-17.10>
2019-04-17 13:56:13 -0400

    Made some changes.

--- Python/gradient.py
+++ Python/gradient.py
@@ -10,15 +10,21 @@ def gradient(ys, xs, order = 1) :
     a list of vectors. Can compute any order up to the number of y values
     less one.
     """
-    outs = []
-    for i in range(0, order) :
-        for j in range(0, len(ys)) :
+    if hasattr(xs[0], "__getitem__") :
+        outs = []
+        for i in range(0, len(ys)) :
             grad = []
-            for k in range(0, len(xs[0])) :
-                # Set up constant array.
-                const_array = np.array([0 for i in range(0, len(ys))])
-                const_array[i + 1] = math.factorial(i)
-
-                #Set up coefficient array.
-                coeff_array =
-                np.array([[
+            outs.append(np.array(grad))
+        return outs
+    else :
+        outs = []
+        for j in range(0, len(ys)) : 
+            const_array = np.array([0 for i in range(0, len(ys))])
+            const_array[1] = math.factorial(i)
+            coeff_array = \
+                    np.array([[(xs[l] - xs[j]) ** m for l in range(0, len(xs))]
+                              for m in range(0, len(xs))])
+            sol = np.linalg.solve(coeff_array, const_array)
+            outs.append(sum(sol[i] * ys[i] for i in range(0, len(sol))))
+        return (outs)
+    
--- Python/machine_learning.py
+++ Python/machine_learning.py
@@ -65,13 +65,25 @@ not be looping forever.
     else :
         w1 = weight_guess.copy()
     w2 = None
-    wdiff = np.array([random.random() for i in w1])
-    g1 = gradient(lambda w : loss(regression, w, inputs, outputs), w1, wdiff)
+    wdiff = np.array([2 * random.random() - 1 for i in w1])
+    try :
+        g1 = gradient(lambda w : loss(regression, w, inputs, outputs), w1, wdiff)
+    except Exception as err:
+        print("Error! Bad division!")
+        print(f"w1: {w1}\nwdiff: {wdiff}")
+        print(err.__traceback__)
+        raise(err)
     g2 = None
     w2 = w1
     w1 = w1 - 0.001 * g1;
     g2 = g1;
-    g1 = gradient(lambda w : loss(regression, w, inputs, outputs), w1, w1 - w2)
+    try :
+        g1 = gradient(lambda w : loss(regression, w, inputs, outputs), w1, w1 - w2)
+    except Exception as err:
+        print("Error! Bad division!")
+        print(f"w1: {w1}\nw2: {w2}\ng1: {g1}\ng2: {g2}")
+        print(err.__traceback__)
+        raise(err)
     count = 0
     while (math.sqrt((w1 - w2).dot(w1 - w2)) / (len(w1) ** 2) > eps or
            g1.dot(g1) > eps or g2.dot(g2) > eps) and count < 1/eps :
@@ -82,26 +94,77 @@ not be looping forever.
         w2 = w1
         g2 = g1
         w1 = w1 - change * g1
-        g1 = gradient(lambda w : loss(regression, w, inputs, outputs),
-                      w1, w1 - w2)
+        try :
+            g1 = gradient(lambda w : loss(regression, w, inputs, outputs),
+                          w1, w1 - w2)
+        except Exception as err :
+            print("Error! Bad division!")
+            print(f"w1: {w1}\nw2: {w2}\ng1: {g1}\ng2: {g2}\ncount: {count}")
+            print(err.__traceback__)
+            raise(err)
         count += 1
-    return Bot(regression, w1)
+    return w1
+
+def cross_validation(regression, weight_guess, inputs, outputs, k = 10,
+                     gradient = lambda f, w1, wdiff :
+          np.array([(f([w1[j] + (wdiff[j] if i == j else 0) for j in
+                        range(0, len(w1))])
+            - f(w1)) / wdiff[i] for i in range(0, len(w1))]),
+          loss = lambda f, w, ins, outs :
+          sum([(f(w, ins[i]) - outs[i]) ** 2 for i in range(0, len(ins))])
+          / len(ins) ** 2,
+          step = 0.001, eps = 0.00001) :
+    ins = inputs
+    outs = outputs
+    rem_ins = np.array_split(ins, k)
+    rem_outs = np.array_split(outs, k)
+    weights = []
+    weight = weight_guess
+    mles = []
+    mle = 0
+    for i in range(k) :
+        train_in = [ins[j] for j in range(len(ins)) if list(rem_ins[i]).count(ins[j]) == 0]
+        train_out = [outs[j] for j in range(len(outs)) if list(rem_outs[i]).count(outs[j]) == 0]
+        val_ins = rem_ins[i]
+        val_outs = rem_outs[i]
+        weight = teach(regression, weight, train_in, train_out, gradient, loss, step, eps)
+        weights.append(weight)
+        mle = loss(regression, weight, val_ins, val_outs)
+        mles.append(mle)
+    return weight, mle, weights, mles
+        
 
 #Run tests
-if __name__ == "__main__" :
+def main() :
+    import matplotlib.pyplot as plt
+    import matplotlib.markers as mark
+    from mpl_toolkits.mplot3d import Axes3D as plt3d
+
+    #np.seterr(all="raise")
+
+    r = lambda w, in_ : w[0] * in_[0] + w[1]
+    r2 = lambda w, in_ : w[0] * in_[0] ** 2 + w[1] * in_[1] ** 2 + \
+         w[2] * in_[0] * in_[1] + w[3] * in_[0] + w[4] * in_[1] + w[5]
+    
     input1 = [[0], [1], [2], [3], [4]]
+    input4 = [[random.random(), random.random()] for i in range(500)]
     output1 = [0, 1, 2, 3, 4]
     output2 = [1, 3, 5, 7, 9]
     output3 = [0]
+    weight4 = [random.random() for i in range(6)]
+    output4 = list(map(lambda x : r2(weight4, x), input4))
 
-    r = lambda w, in_ : w[0] * in_[0] + w[1]
+
     bot1 = Bot(r, [1, 0])
     bot2 = Bot(r, [2, 1])
+    bot4 = Bot(r2, weight4)
 
-    bot_test_1 = teach(r, [0, 0], input1, output1)
-    bot_test_2 = teach(r, [0, 0], input1, output2)
+    bot_test_1 = Bot(r, cross_validation(r, [0, 0], input1, output1, k = 3, step = 0.00001, eps = 0.000001)[0])
+    bot_test_2 = Bot(r, teach(r, [0, 0], input1, output2))
+    bot_test_4 = Bot(r2, teach(r2, [0 for i in range(6)],
+                               input4, output4, eps = 0.0001))
     try :
-        bot_test_3 = teach(r, [0, 0], input1, output3)
+        bot_test_3 = Bot(r, teach(r, [0, 0], input1, output3))
     except AssertionError :
         pass;
     else :
@@ -117,6 +180,53 @@ if __name__ == "__main__" :
         print("Failed.")
     else :
         print("Succeeded")
-    s = input("Press any key to continue")
-    if s != None :
-        exit()
+    plt.figure()
+    plt.title("Bot 1")
+
+    avg = sum(output2) / len(output2)
+    sum_exp = sum((o - avg) ** 2 for o in output1)
+    sum_reg = sum((bot_test_2.compute(i) - avg) ** 2 for i in input1)
+    
+    plt.scatter([input1[i][0] for i in range(len(input1))], output1,
+                marker = mark.MarkerStyle("o"))
+    plt.plot([input1[i][0] for i in range(len(input1))],
+             [bot1.compute(i) for i in input1], "b--", label = "Expected Bot")
+    plt.plot([input1[i][0] for i in range(len(input1))],
+             [bot_test_1.compute(i) for i in input1], "r--",
+             label = f"Computed Bot, w = {bot_test_1.get_weights()}, r² = {sum_reg / sum_exp}")
+    plt.legend()
+
+    
+    plt.figure()
+
+    avg = sum(output2) / len(output2)
+    sum_exp = sum((o - avg) ** 2 for o in output2)
+    sum_reg = sum((bot_test_2.compute(i) - avg) ** 2 for i in input1)
+    
+    plt.title("Bot 2")
+    plt.scatter([input1[i][0] for i in range(len(input1))], output2,
+                marker = mark.MarkerStyle("o"))
+    plt.plot([input1[i][0] for i in range(len(input1))],
+             [bot2.compute(i) for i in input1], "b--", label = "Expected Bot")
+    plt.plot([input1[i][0] for i in range(len(input1))],
+             [bot_test_2.compute(i) for i in input1], "r--",
+             label = f"Computed Bot, w = {bot_test_2.get_weights()}, r² = {sum_reg / sum_exp}")
+    plt.legend()
+    plt.figure()
+
+    avg = sum(output4) / len(output4)
+    sum_exp = sum((o - avg) ** 2 for o in output4)
+    sum_reg = sum((r2(bot_test_4.get_weights(), input4[i]) - avg) ** 2
+                  for i in range(len(output4)))
+    ax = plt.gca(projection="3d")
+    ax.scatter(np.array([v[0] for v in input4]),
+                  np.array([v[1] for v in input4]), np.array(output4))
+    X, Y = np.meshgrid([v[0] for v in input4], [v[1] for v in input4])
+    Z = np.array([[r2(bot_test_4.get_weights(), [X[i][j], Y[i][j]])
+          for j in range(len(X[i]))] for i in range(len(X))])
+    ax.plot_surface(X, Y, Z, rcount=25, ccount=25)
+    plt.title(f"Bot Calculated: r² = {sum_reg / sum_exp}")
+    plt.show()
+
+if __name__ == "__main__" :
+    main()
